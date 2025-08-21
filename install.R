09765df8324eff015ecd9a7939e27ce0824e677c
Abner Bogan <abnerbog@buffalo.edu>
2025-08-21 16:23:04 -0400

    Update pkg installation instructions

--- install.R
+++ install.R
@@ -1,14 +1,14 @@
 # These R packages can also be found in anaconda: https://repo.anaconda.com/pkgs/r/
 install.packages("tidyverse")
-install.packages("dataretrieval")
+install.packages("dataRetrieval")
 install.packages("targets")
 install.packages("httr2")
-install.packages("sf")
+install.packages("sf", type = "source")
 install.packages("xml2")
 install.packages("leaflet")
 install.packages("rcolorbrewer")
 install.packages("htmlwidgets")
-install.packages("tigris")
+install.packages("tigris", type = "source")
 install.packages("zip")
 # This R package is not found in anaconda; this is why we are using an install.R
 # file instead of environment.yml -- see https://github.com/binder-examples/r-conda 
