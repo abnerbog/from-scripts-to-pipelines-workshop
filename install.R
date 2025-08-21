a2d1949c745f503787a24d4477646eff86d5e2d3
Abner Bogan <abnerbog@buffalo.edu>
2025-08-21 15:40:28 -0400

    Create `install.R` file with pkg dependencies

---
+++ install.R
@@ -0,0 +1,16 @@
+# These R packages can also be found in anaconda: https://repo.anaconda.com/pkgs/r/
+install.packages("tidyverse")
+install.packages("dataretrieval")
+install.packages("targets")
+install.packages("httr2")
+install.packages("sf")
+install.packages("xml2")
+install.packages("leaflet")
+install.packages("rcolorbrewer")
+install.packages("htmlwidgets")
+install.packages("tigris")
+install.packages("zip")
+# This R package is not found in anaconda; this is why we are using an install.R
+# file instead of environment.yml -- see https://github.com/binder-examples/r-conda 
+# for more information
+install.packages("tools")
