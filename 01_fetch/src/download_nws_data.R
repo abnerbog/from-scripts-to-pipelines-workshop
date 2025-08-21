86e88bea23a600a15f86848eab03a84346be808f
Abner Bogan <abnerbog@buffalo.edu>
2025-08-21 16:40:13 -0400

    Add example roxygen documentation

--- 01_fetch/src/download_nws_data.R
+++ 01_fetch/src/download_nws_data.R
@@ -1,3 +1,22 @@
+#' @title Download flood stage data from the National Weather Service
+#' @description This function downloads current flood stage information from the 
+#' National Weather Service (NWS) API for a specified list of U.S. Geological Survey 
+#' (USGS) sites. It uses a provided cross-reference table to convert the USGS site
+#' numbers to their corresponding NWS IDs, then loops through each NWS ID to make
+#' an API request. The function returns a tibble with the original site data along 
+#' with the newly retrieved flood stage and units.
+#' 
+#' @param nws_conversion A data.frame or tibble that acts as a cross-reference 
+#' between USGS and NWS site identifiers. It must contain at least two columns: 
+#' a USGS column with USGS site numbers and a NWS column with the corresponding 
+#' NWS IDs.
+#' @param sites A character vector of USGS site numbers for which to download flood
+#'stage data.
+#' 
+#' @return A tibble with a row for each site requested. The output includes the 
+#' original site_no column along with new columns for the corresponding NWS ID, 
+#' flood stage (the value of the minor flood stage), and stage units.
+#' 
 download_nws_data <- function(nws_conversion, sites){
 
   sites <- tibble("site_no"=sites)
